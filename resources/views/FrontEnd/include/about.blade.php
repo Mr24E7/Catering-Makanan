9ea4b9960d8b1a9f4eec8887bc0ac90f79168c3d
Ridha <mr240102e7@gmail.com>
2022-06-07 10:30:02 +0800

    Menambah About

--- resources/views/FrontEnd/include/about.blade.php
+++ resources/views/FrontEnd/include/about.blade.php
@@ -20,33 +20,15 @@
 		<div class="container ">
 			<h3 class="w3ls-title w3ls-title1">About Us</h3>
 			<div class="about-text">
-Tutorial run this project on your computer
-1. Clone this repo (https://github.com/Mr24E7/Catering-Makanan.git) or Download ZIP (don't forget to extract)
-2. Go to your localhost/phpmyadmin/ and make database name bookutu
-3. Open Visual Studio Code and XAMPP (Start Apache and MySQL)
-4. Open the folder on VSC
-5. Run terminal
-6. Type "php artisan migrate"
-7. Go to your localhost/phpmyadmin/
-8. Delete table book in database bookutu
-9. Select import in phpmyadmin
-10. Choose file: books.sql
-11. Then click go
-12. Back to visual studio code and run the terminal
-13. Type "php artisan server"
-14. Go to browser and type 127.0.0.1
-15. First, make account and login.
-16. End
 
-Website ini dibuat menggunakan
-1. Web Framework: Laravel
-2. Javascript Framework: OWL Carousel dan jQuery
-3. UI Framework: Bootstrap
+<p>Website ini dibuat menggunakan</p>
+<p>1. Web Framework: Laravel</p>
+<p>2. Javascript Framework: OWL Carousel dan jQuery</p>
+<p>3. UI Framework: Bootstrap</p>
 
-Tugas ini dibuat oleh:
-1. Zhia Amirul Mukminin dan 
-2. Muhammad Ridha
-					Untuk menjadi mitra bagi klien kami dengan integritas tertinggi, perhatian terhadap detail, praktik, dan standar, dengan tujuan akhir melebihi harapan klien kami dan tamu mereka</p>
+<p>Tugas ini dibuat oleh:</p>
+<p>1. Zhia Amirul Mukminin dan </p>
+<p>2. Muhammad Ridha</p>
 				<div class="ftr-toprow">
 					<div class="col-md-4 ftr-top-grids">
 						<div class="ftr-top-left">
